cad0f23c1ef2f61ca507219d2eedd0d948a7cc04
isabelle george and kevin mattison <mattisonii@yahoo.com>
2016-02-16 15:43:27 -0800

    create website for tamagotchi app

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,6 +9,15 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
 
+    get("/", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+
+      model.put("tasks", request.session().attribute("tasks"));
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
